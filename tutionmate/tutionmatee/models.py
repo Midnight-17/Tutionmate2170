fde6c4b11fdec9e54d00150730375c94915f7ec7
Midnight-17 <nishanth7861@gmail.com>
2025-04-06 09:46:32 +0800

    added rates

--- tutionmate/tutionmatee/models.py
+++ tutionmate/tutionmatee/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class subjects(models.Model):
     type = models.CharField(max_length=100)
@@ -11,7 +12,20 @@ class subjects(models.Model):
 class teacher(models.Model):
     name = models.CharField(max_length=100)
     subjects = models.ManyToManyField(subjects, related_name="subjects")
-    age = models.IntegerField(null=True, blank= True)
+    rate_min = models.BigIntegerField(
+        null=True, blank=True, 
+        validators=[MinValueValidator(0), MaxValueValidator(9999)]  # Min value 0, Max value 5000
+    )
+    rate_max = models.IntegerField(
+        null=True, blank=True, 
+        validators=[MinValueValidator(0), MaxValueValidator(10000)]  # Min value 0, Max value 10000
+    )
+
+    def validate_rate(self):
+    # Custom validation: rate_max should always be greater than rate_min
+        if self.rate_min is not None and self.rate_max is not None:
+            if self.rate_max < self.rate_min:
+                raise ValidationError({'rate_max': 'rate_max cannot be less than rate_min.'})
 
     def __str__(self):
         return self.name
